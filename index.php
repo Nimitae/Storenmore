f2e33e432af51de5cd558e656d6283388c451765
thestoner37 <alvinalim.37@gmail.com>
2015-07-27 20:57:04 +0800

    colour changes

--- index.php
+++ index.php
@@ -6,9 +6,7 @@ include("partials/header.partial.php");
 
 ?>
 <br><br>
-<div class="centeredImage">
-    <img src="http://media.playpark.net/MapleStory/uploadimages/Wallpaper_BH1_1600x864.jpg" height="75%" width="75%">
-</div>
+
 
 <div style="width: 80%; margin:auto">
 
@@ -17,18 +15,18 @@ include("partials/header.partial.php");
     </h2>
 
     <div class="col-sm-12">
-        <div class="col-sm-3">
+        <div class="col-sm-4">
             <div class="panel panel-default">
                 <div class="panel-heading">Title of goods</div>
-                <div class="panel-body">Description of goods</div>
+                <div class="panel-body"><img src="http://i57.tinypic.com/v5eux5.jpg"></div>
                 <div class="panel-footer"> Price: ##</div>
             </div>
         </div>
 
-        <div class="col-sm-3">
+        <div class="col-sm-5">
             <div class="panel panel-default">
                 <div class="panel-heading">Title of goods</div>
-                <div class="panel-body">Description of goods</div>
+                <div class="panel-body"><img src="http://i57.tinypic.com/v5eux5.jpg"> </div>
                 <div class="panel-footer"> Price: ##</div>
             </div>
         </div>

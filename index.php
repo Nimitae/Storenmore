61c7cc51c3743d87b793ad3380c01737dcf5d1ba
thestoner37 <alvinalim.37@gmail.com>
2015-07-27 16:16:31 +0800

    changed nav bar

--- index.php
+++ index.php
@@ -9,6 +9,37 @@ include("partials/header.partial.php");
 
 ?>
 <br><br>
-<div class="container">
-    Welcome to StoreNMore!
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-header" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                </button>
+
+                <a class="navbar-brand" href="#" style="color:white">thebackyard</a>
+            </div>
+
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#" style="color:white">BROWSE</a>
+                    </li>
+                    <li>
+                        <a href="#" style="color:white">SELL</a>
+                    </li>
+                <li>
+                <form action="home.php" class="navbar-form navbar-right">
+                    <input type="text" class="form-control" placeholder="Search Modules...">
+                </form>
+                    </li>
+                    </ul>
+            </div>
+
+
+       
+
+    </div>
 </div>

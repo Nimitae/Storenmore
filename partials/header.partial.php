3eb11042c99fd66b26b90fea4348c2868e30af84
Nimitae <nimitae@hotmail.com>
2015-07-27 12:43:38 +0800

    Fixes

--- partials/header.partial.php
+++ partials/header.partial.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="/bootstrap-3.3.5-dist/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.css">
-    <link rel="stylesheet" type="text/css" href="/main.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap-3.3.5-dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap-3.3.5-dist/css/bootstrap-theme.css">
+    <link rel="stylesheet" type="text/css" href="main.css">
     <style>
         ul {
             list-style-type: none;
@@ -23,22 +23,22 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="/">StoreNMore</a>
+            <a class="navbar-brand" href="">StoreNMore</a>
         </div>
         <div>
             <ul class="nav navbar-nav">
-                <li><a class="nav-text" href="/equipment">EQUIPMENT</a></li>
-                <li><a class="nav-text" href="/usables">USABLES</a></li>
-                <li><a class="nav-text" href="/sell">SELL TO US</a></li>
+                <li><a class="nav-text" href="equipment">EQUIPMENT</a></li>
+                <li><a class="nav-text" href="usables">USABLES</a></li>
+                <li><a class="nav-text" href="sell">SELL TO US</a></li>
             </ul><ul class="pull-right nav navbar-nav">
             <?php if (isset($_SESSION['username'])) : ?>
 
-                <li><a class="nav-text" href="/equipment/admin.php">EQUIPMENT(ADMIN)</a></li>
-                <li><a class="nav-text" href="/crafting/admin.php">CRAFTING(ADMIN)</a></li>
-                <li><a class="nav-text" href="/usables/admin.php">USABLES(ADMIN)</a></li>
+                <li><a class="nav-text" href="equipment/admin.php">EQUIPMENT(ADMIN)</a></li>
+                <li><a class="nav-text" href="crafting/admin.php">CRAFTING(ADMIN)</a></li>
+                <li><a class="nav-text" href="usables/admin.php">USABLES(ADMIN)</a></li>
 
             <?php endif; ?>
-                <li><a class="nav-text" href="/chat">TALK TO US</a></li>
+                <li><a class="nav-text" href="chat">TALK TO US</a></li>
             </ul>
         </div>
     </div>

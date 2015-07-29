678cbd12ce46ba17d67739ade4cde8b812114b06
thestoner37 <alvinalim.37@gmail.com>
2015-07-29 14:06:53 +0800

    created profile.php

--- buysell.php
+++ buysell.php
@@ -9,119 +9,34 @@ include("partials/header.partial.php");
 
 <div class="container">
     <div class="row">
-        <div class="col-sm-2 sell-panel">
-            <div class="panel panel-info">
-                <div class="panel-heading">Purple Cube</div>
-                <div class="panel-body">
-                    <div class="usable-image centeredImage">
-                        <a href=""><img src="images/purplecube.png"></a>
+        <a href="">
+            <div class="col-sm-2 sell-panel">
+                <div class="panel panel-info">
+                    <div class="panel-heading">Purple Cube</div>
+                    <div class="panel-body">
+                        <div class="usable-image centeredImage">
+                            <img src="images/purplecube.png">
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-sm-2 sell-panel">
-            <div class="panel panel-info">
-                <div class="panel-heading">Yellow Cube</div>
-                <div class="panel-body">
-                    <div class="usable-image centeredImage">
-                        <a href=""><img src="images/yellowcube.png"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-2 sell-panel">
-            <div class="panel panel-info">
-                <div class="panel-heading">Clean Slate Scroll</div>
-                <div class="panel-body">
-                    <div class="usable-image centeredImage">
-                        <a href=""><img src="images/css.png"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-2 sell-panel">
-            <div class="panel panel-info">
-                <div class="panel-heading">Safety Scroll</div>
-                <div class="panel-body">
-                    <div class="usable-image centeredImage">
-                        <a href=""><img src="images/safety.png"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-2 sell-panel">
-            <div class="panel panel-info">
-                <div class="panel-heading">Chaos of Goodness</div>
-                <div class="panel-body">
-                    <div class="usable-image centeredImage">
-                        <a href=""><img src="images/chaos.png"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </a>
     </div>
+
+    <!-- ETCS -->
+
     <div class="row">
-        <div class="col-sm-2 sell-panel">
-            <div class="panel panel-warning">
-                <div class="panel-heading">Twisted Time</div>
-                <div class="panel-body">
-                    <div class="usable-image centeredImage">
-                        <a href=""><img src="images/twistedtime.png"></a>
+        <a href="">
+            <div class="col-sm-2 sell-panel">
+                <div class="panel panel-warning">
+                    <div class="panel-heading">Twisted Time</div>
+                    <div class="panel-body">
+                        <div class="usable-image centeredImage">
+                            <img src="images/twistedtime.png">
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-sm-2 sell-panel">
-            <div class="panel panel-warning">
-                <div class="panel-heading">High Spell Essence</div>
-                <div class="panel-body">
-                    <div class="usable-image centeredImage">
-                        <a href=""><img src="images/highspell.png"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-2 sell-panel">
-            <div class="panel panel-warning">
-                <div class="panel-heading">Time Rock</div>
-                <div class="panel-body">
-                    <div class="usable-image centeredImage">
-                        <a href=""><img src="images/timerock.png"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-2 sell-panel">
-            <div class="panel panel-warning">
-                <div class="panel-heading">Dream Fragment</div>
-                <div class="panel-body">
-                    <div class="usable-image centeredImage">
-                        <a href=""><img src="images/dream.png"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-2 sell-panel">
-            <div class="panel panel-warning">
-                <div class="panel-heading">Confusion Frag</div>
-                <div class="panel-body">
-                    <div class="usable-image centeredImage">
-                        <a href=""><img src="images/confusion.png"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-2 sell-panel">
-            <div class="panel panel-warning">
-                <div class="panel-heading">Primal Essence</div>
-                <div class="panel-body">
-                    <div class="usable-image centeredImage">
-                        <a href=""><img src="images/primal.png"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </a>
     </div>
 </div>
---
+++ profile.php
@@ -0,0 +1,9 @@
+<?php
+require_once("forAllPages.php");
+
+
+include("partials/header.partial.php");
+
+?>
+
+
